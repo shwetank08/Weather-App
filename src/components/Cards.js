c63f58d161e30cec7ed8f80711d8b9919fe47c5d
shwetank08 <shwetanksingh.in@gmail.com>
2023-01-04 20:09:29 +0530

    added style

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,95 +1,102 @@
-import {
-  ThermometerLow,
-  ThermometerHigh,
-  GeoAltFill,
-} from "react-bootstrap-icons";
-
 import React, { useEffect, useState } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Alert } from "react-bootstrap";
+
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+
 const { REACT_APP_API } = process.env;
 
+
 const Cards = ({ city }) => {
+  city = city.toUpperCase();
   const [weather, setWeather] = useState({
     temp: "",
-    min: "",
-    max: "",
     wind: "",
     humidity: "",
     feelsLike: "",
-    description: "",
+    desc: "",
+    imageUrl: "",
   });
 
   const fetchCityWeather = async () => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${REACT_APP_API}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        let { feels_like, temp, temp_max, temp_min, humidity } = data.main;
-        const { description } = data.weather[0];
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${REACT_APP_API}`
+      );
 
-        feels_like -= 273;
-        temp -= 273;
-        temp_max -= 273;
-        temp_min -= 273;
+      const data = await res.json();
 
-        feels_like = feels_like.toFixed(2);
-        temp = temp.toFixed(2);
-        temp_max = temp_max.toFixed(2);
-        temp_min = temp_min.toFixed(2);
+      let { feels_like, temp, humidity } = data.main;
+      const { description, icon } = data.weather[0];
+      const { speed } = data.wind;
 
-        setWeather({
-          temp: temp,
-          min: temp_min,
-          max: temp_max,
-          humidity: humidity,
-          feelsLike: feels_like,
-          description: description,
-        });
+      feels_like -= 273;
+      temp -= 273;
 
-        // console.log(feels_like, temp, temp_max, temp_min, description);
+      feels_like = feels_like.toFixed(2);
+      temp = temp.toFixed(2);
+
+      let desc = description.toUpperCase();
+
+      setWeather({
+        temp: temp,
+        wind: speed,
+        humidity: humidity,
+        feelsLike: feels_like,
+        desc: desc,
+        imageUrl: `http://openweathermap.org/img/w/${icon}.png`,
       });
+    } catch (err) {
+      return (<Alert variant="warning">City Not Found</Alert>)
+    }
   };
 
   useEffect(() => {
     fetchCityWeather();
-  }, []);
+  }, [city]);
 
   return (
     <div className="container text-center">
       <div
         className="d-flex justify-content-between"
-        style={{ color: "#ffffff" }}
+        style={{ color: "#000000", marginTop: "2rem" }}
       >
         <div>
-          <div>{city}</div>
-          <div>{weather.temp}&deg;C</div>
+          <div>
+            <h1>{city}</h1>
+          </div>
+          <div style={{ textSize: "50px" }}>
+            <h1>{weather.temp}&deg;C</h1>
+          </div>
         </div>
         <div>
-          <h2 style={{ color: "#ffffff" }}>{weather.description}</h2>
+          <img src={weather.imageUrl} alt="weather icon" />
+          <h2 style={{ color: "#000000" }}>{weather.desc}</h2>
         </div>
       </div>
-      <div className="d-flex sm:flex-wrap justify-content-around" style={{marginTop: "22rem", color:"#ffffff"}}>
-        <Card style={{ width: "18rem", background: "rgba(255,255,255, 0.2)" }}>
+      <div
+        className="d-flex border-0 sm:flex-wrap justify-content-around"
+        style={{ marginTop: "20%", color: "#000000" }}
+      >
+        <Card
+          className="border-0"
+          style={{ width: "18rem", background: "rgba(255,255,255, 0.2)" }}
+        >
           <Card.Body>
             <Card.Title>FEELS LIKE</Card.Title>
             <Card.Text>{weather.feelsLike}&deg;C</Card.Text>
           </Card.Body>
         </Card>
-        <Card style={{ width: "18rem", background: "rgba(255,255,255, 0.2)" }}>
+        <Card className="border-0" style={{ width: "18rem" }}>
           <Card.Body>
-            <Card.Title>MAX</Card.Title>
-            <Card.Text>{weather.min}&deg;C</Card.Text>
+            <Card.Title>WIND</Card.Title>
+            <Card.Text>{weather.wind} MPH</Card.Text>
           </Card.Body>
         </Card>
-        <Card style={{ width: "18rem", background: "rgba(255,255,255, 0.2)" }}>
-          <Card.Body>
-            <Card.Title>MIN</Card.Title>
-            <Card.Text>{weather.min}&deg;C</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem", background: "rgba(255,255,255, 0.2)" }}>
+        <Card
+          className="border-0"
+          style={{ width: "18rem", background: "rgba(255,255,255, 0.2)" }}
+        >
           <Card.Body>
             <Card.Title>HUMIDITY</Card.Title>
             <Card.Text>{weather.humidity}%</Card.Text>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
+
 import { InputGroup, Container, Form } from "react-bootstrap";
+import {ToastContainer} from 'react-toastify'
+
 import Cards from "./Cards";
+
 const Weather = () => {
   const [city, setCity] = useState("New Delhi");
+  const [inp, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    if(e.key === "Enter"){
+      setCity(inp);
+    }
+  }
+
   return (
     <Container>
+    <ToastContainer/>
       <div className="row m-3 p-3">
-        <InputGroup className="mb-3 border-0" style={{ width: '18rem', marginLeft: 'auto', marginRight: 'auto' }}>
+        <InputGroup className="mb-3" style={{ width: '18rem', marginLeft: 'auto', marginRight: 'auto', borderColor:'#000000' }}>
           <Form.Control
             className="text-center"
             placeholder="Enter City"
-            onChange={(e) => setCity(e.target.value)}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={handleSubmit}
           />
         </InputGroup>
       </div>
